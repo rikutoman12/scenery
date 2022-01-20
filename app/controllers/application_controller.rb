5b467ddb4eaa1b9b27d10b45d9b450be50765916
rikutoman12 <rikutoo1312@gmail.com>
2022-01-20 07:59:02 +0000

    新規ログイン完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
-  include SessionsHelper
+  helper_mathod :current_user
+    def login?
+        if current_user.nil?
+            redirect_to login_path, alert: "you have to login"
+        end
+    end
+
+    def already_login?
+        unless current_user.nil?
+            redirect_to user_path, notice: "you are already login"
+        end
+    end
+
+    def current_user
+        if session[:user_id]
+            current_user ||= User.find(session[:user_id])
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  resource :user, only: [:new, :create, :show]
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    '/login',   to: 'sessions#new'

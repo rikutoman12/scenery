d271c82c29b769e4796d362bf3902154ac288994
rikutoman12 <rikutoo1312@gmail.com>
2022-01-28 03:43:27 +0000

    コメント機能完成

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,6 +1,6 @@
 class PostCommentsController < ApplicationController
 
-   def create
+  def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
@@ -10,6 +10,8 @@ class PostCommentsController < ApplicationController
 
 
   def destroy
+    PostComment.find_by(id: params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
